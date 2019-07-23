5b9a2c8fd3ccd5dad5a6a7acafce50fe232b993a
Boris Timofeev <mashin87@gmail.com>
2019-07-23 13:43:56 +0300

    fix deprecated method

--- app/src/main/java/org/emunix/unipatcher/ui/activity/HelpActivity.java
+++ app/src/main/java/org/emunix/unipatcher/ui/activity/HelpActivity.java
@@ -54,7 +54,7 @@ public class HelpActivity extends AppCompatActivity {
         final PagerAdapter adapter = new HelpPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
