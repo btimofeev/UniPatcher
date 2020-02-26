6ac0fa1a87521e47fbc8f05bc3f1b1bc04b5b3da
Boris Timofeev <mashin87@gmail.com>
2020-02-26 16:17:58 +0300

    convert settings activity and fragment to kotlin

---
+++ app/src/main/java/org/emunix/unipatcher/ui/activity/SettingsActivity.kt
@@ -0,0 +1,50 @@
+/*
+Copyright (C) 2016, 2020 Boris Timofeev
+
+This file is part of UniPatcher.
+
+UniPatcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UniPatcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UniPatcher.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.emunix.unipatcher.ui.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import org.emunix.unipatcher.R
+import org.emunix.unipatcher.ui.fragment.SettingsFragment
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.content, SettingsFragment())
+                .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/org/emunix/unipatcher/ui/fragment/SettingsFragment.kt
@@ -0,0 +1,60 @@
+/*
+Copyright (C) 2016-2017, 2019 Boris Timofeev
+
+This file is part of UniPatcher.
+
+UniPatcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+UniPatcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with UniPatcher.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.emunix.unipatcher.ui.fragment
+
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
+import org.emunix.unipatcher.R
+import org.emunix.unipatcher.helpers.ThemeHelper.applyTheme
+
+class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeListener {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences,
+                                           key: String) {
+        when (key) {
+            "output_directory" -> {
+                Toast.makeText(activity, R.string.settings_theme_message_restart_app, Toast.LENGTH_SHORT).show()
+            }
+            "theme" -> {
+                val theme = findPreference<ListPreference>("theme")
+                if (theme?.value != null)
+                    applyTheme(theme.value)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences
+                .registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences
+                .unregisterOnSharedPreferenceChangeListener(this)
+    }
+}
